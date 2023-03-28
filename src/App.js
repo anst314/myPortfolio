8aee1c43e9d32e1eafeb4517b206d58ea70de5ca
Anthony Stanton <anthonystanton314@gmail.com>
2023-03-28 15:36:03 -0400

    first deploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './components/Home'
+import Projects from './components/Projects'
+import About from './components/About'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return ( 
+        <div className="App">
+            <Router>
+                <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/ projects" element={<Projects />} />
+                <Route path="/ About" element={<Home />} />
+                </Routes>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
