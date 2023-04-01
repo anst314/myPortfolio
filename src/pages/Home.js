13ed23317b84ddbf52173dfbb9511ade0a3dbe4c
Anthony Stanton <anthonystanton314@gmail.com>
2023-04-01 12:03:05 -0400

    removed audio section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,9 +4,9 @@ import audio from '../images/hiphop-lofi-music.m4a'
 function Home() {
   return (
     <div>
-        <audio controls>
+        {/* <audio controls> */}
   {/* <source src={audio} type="audio/*"/> */}
-    </audio>
+    {/* </audio> */}
         <iframe width="700" height="500" allow="autoplay" src="https://player.vimeo.com/video/553617869?title=0&portrait=0&byline=0&autoplay=1&loop=1&transparent=1" title="description"></iframe>
 
     </div>
